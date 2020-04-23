fdc4f7b48efe9969e2b5aeb42e42082ad0018fd2
Melissa Aitkin <melissaaitkin@gmail.com>
2020-04-23 12:55:03 -0400

    add not applicable category

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -13,6 +13,7 @@ function get_country_names()
         $country_names[] = 'Wales';
         $country_names[] = 'Northern Ireland';
         $country_names[] = 'Multiple';
+        $country_names[] = 'Not Applicable';
         asort($country_names);
         array_unshift($country_names, 'Please Select');
         Cache::put('country_names', $country_names, 10080);
