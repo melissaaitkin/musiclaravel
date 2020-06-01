50b96b6f6b85e6d9afc845f2fe4b83007bc66cc9
Melissa Aitkin <melissaaitkin@gmail.com>
2020-06-01 17:01:58 -0400

    using redis for caching

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -2,10 +2,10 @@
 
 function get_country_names() 
 {
-    if (!Cache::has('country_names')){
-        $countries = json_decode( file_get_contents( "https://restcountries.eu/rest/v2/all") );
+    if (!Cache::store('redis')->has('country_names')) {
+        $countries = json_decode(file_get_contents( "https://restcountries.eu/rest/v2/all"));
         $country_names = [];
-        foreach( $countries as $country ) {
+        foreach($countries as $country) {
             $country_names[] = $country->name;
         }
         $country_names[] = 'England';
@@ -16,7 +16,7 @@ function get_country_names()
         $country_names[] = 'Not Applicable';
         asort($country_names);
         array_unshift($country_names, 'Please Select');
-        Cache::put('country_names', $country_names, 10080);
+        Cache::store('redis')->put('country_names', $country_names, 10080);
     }
-    return Cache::get('country_names');
+    return Cache::store('redis')->get('country_names');
 }
