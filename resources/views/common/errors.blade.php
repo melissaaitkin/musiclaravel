c37386c96a379a787a3a26fe7b3724769d1f333f
Melissa Aitkin <melissaaitkin@gmail.com>
2020-06-07 16:59:49 -0400

    change syntax for error to enable html messages be displayed

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -7,7 +7,7 @@
 
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>{!! $error !!}</li>
             @endforeach
         </ul>
     </div>
