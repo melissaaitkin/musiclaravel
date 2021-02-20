d3fe63970fb09e1a65b20ce89d1d4b712d516e15
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2021-02-20 13:13:22 -0500

    add a data fix command

---
+++ app/Console/Commands/PerformDataFix.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Music\Song\Song;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+use Log;
+use Storage;
+
+class PerformDataFix extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:df
+                            {--missing-songs : Report on missing songs}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fix db data issues';
+
+    /**
+     * Command options
+     *
+     * @var array
+     */
+    private $options;
+
+    /**
+     * The media directory
+     *
+     * @var string
+     */
+    private $media_directory;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->media_directory = Redis::get('media_directory');
+        parent::__construct();
+    }
+
+    /*
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->options = $this->options();
+
+        if ($this->options['missing-songs']):
+            $this->reportMissingSongs();
+        endif;
+    }
+
+    /**
+     * Serialize previous teams
+     *
+     * @return mixed
+     */
+    private function reportMissingSongs()
+    {
+        foreach (Song::all() as $song):
+            if (! Storage::disk(config('filesystems.partition'))->has($this->media_directory . $song->location)):
+                Log::info($song->artist->artist . ' ' . $song->title . ' LOCATION: ' . $song->location . ' does not exist');
+            endif;
+        endforeach;
+    }
+
+}
