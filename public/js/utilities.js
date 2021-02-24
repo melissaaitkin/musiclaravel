6a615be19c44722e8d5e80f3ddad2eeb2281a985
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2021-02-24 18:21:23 -0500

    update formatting

--- public/js/utilities.js
+++ public/js/utilities.js
@@ -1,71 +1,71 @@
 function display_jukebox(title, songs) {
-	let song_url = APP_URL + '/song/play/';
+    let song_url = APP_URL + '/song/play/';
 
-	let jukebox_form = '<div class="audio">';
-	jukebox_form += '<figure>';
-	jukebox_form += '<audio controls src="' + song_url + songs[0].id + '">Your browser does not support the<code>audio</code> element.</audio>';
-	jukebox_form += '</figure>';
-	jukebox_form += '<button class="next">Next</button>';
+    let jukebox_form = '<div class="audio">';
+    jukebox_form += '<figure>';
+    jukebox_form += '<audio controls src="' + song_url + songs[0].id + '">Your browser does not support the<code>audio</code> element.</audio>';
+    jukebox_form += '</figure>';
+    jukebox_form += '<button class="next">Next</button>';
 
-	jukebox_form += '<div>';
-	for (i = 0; i < songs.length; i++) {
-		jukebox_form += '<span id="song-' + songs[i].id + '">' + songs[i].title + '</span><br>';
-	}
-	jukebox_form += '</div>';
-	jukebox_form += '</div>';
+    jukebox_form += '<div>';
+    for (i = 0; i < songs.length; i++) {
+        jukebox_form += '<span id="song-' + songs[i].id + '">' + songs[i].title + '</span><br>';
+    }
+    jukebox_form += '</div>';
+    jukebox_form += '</div>';
 
-	$(jukebox_form).dialog({
-	  title: title,
-	  close: function() {
-		$(this).remove()
-	  },
-	  modal: false,
-	  width: 500,
-	  open : function() {
+    $(jukebox_form).dialog({
+      title: title,
+      close: function() {
+        $(this).remove()
+      },
+      modal: false,
+      width: 500,
+      open : function() {
 
-		$('div.ui-dialog').addClass('ui-dialog-jukebox');
+        $('div.ui-dialog').addClass('ui-dialog-jukebox');
 
-		// Remove song that is already set
-		song = songs.shift();
-		// Add css styling
-		let previous_id = song.id;
-		$("#song-" + previous_id).addClass('font-weight-bold');
-		// Play
-		let audio = $(this).find('audio').get(0);
-		audio.play();
+        // Remove song that is already set
+        song = songs.shift();
+        // Add css styling
+        let previous_id = song.id;
+        $("#song-" + previous_id).addClass('font-weight-bold');
+        // Play
+        let audio = $(this).find('audio').get(0);
+        audio.play();
 
-		let next = $(this).find('button.next').get(0);
+        let next = $(this).find('button.next').get(0);
 
-		audio.addEventListener('ended',function() {
-			previous_id = next_song(audio, next, previous_id);
-		});
+        audio.addEventListener('ended',function() {
+            previous_id = next_song(audio, next, previous_id);
+        });
 
-		next.addEventListener('click', function() {
-			previous_id = next_song(audio, next, previous_id);
-		});
+        next.addEventListener('click', function() {
+            previous_id = next_song(audio, next, previous_id);
+        });
 
-		if (songs.length == 0) {
-			$('button.next').hide();
-		}
+        if (songs.length == 0) {
+            $('button.next').hide();
+        }
 
-		function next_song(audio, next, previous_id) {
-			// Get next song
-			song = songs.shift();
-			if (song !== undefined) {
-				audio.src = song_url + song.id;
-				$("#current-song").text(song.title);
-				$("#song-" + previous_id).removeClass('font-weight-bold');
-				previous_id = song.id;
-				$("#song-" + previous_id).addClass('font-weight-bold');
-				audio.pause();
-				audio.load();
-				audio.play();
-				return previous_id;
-			} else {
-				 next.disabled = true;
-			}
-		}
+        function next_song(audio, next, previous_id) {
+            // Get next song
+            song = songs.shift();
+            if (song !== undefined) {
+                audio.src = song_url + song.id;
+                $("#current-song").text(song.title);
+                $("#song-" + previous_id).removeClass('font-weight-bold');
+                previous_id = song.id;
+                $("#song-" + previous_id).addClass('font-weight-bold');
+                audio.pause();
+                audio.load();
+                audio.play();
+                return previous_id;
+            } else {
+                 next.disabled = true;
+            }
+        }
 
-	  }
-	})
+      }
+    })
 }
