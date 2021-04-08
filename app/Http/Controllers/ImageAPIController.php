c1b2da579498c1f85982da0c9675629c0d5e4236
Melissa Aitkin <melissa.aitkin@jacksonriver.com>
2021-04-08 19:17:37 -0400

    save song image path to Redis

--- app/Http/Controllers/ImageAPIController.php
+++ app/Http/Controllers/ImageAPIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Music\Song\Song;
+use Cache;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
@@ -26,18 +27,21 @@ class ImageAPIController extends Controller
     }
 
     public function coverArt($id) {
-        $song = Song::find($id);
-        $location = explode(DIRECTORY_SEPARATOR, $song->location);
-        $location = $this->media_directory . $location[0] . DIRECTORY_SEPARATOR . $location[1];
-        $files = Storage::disk(config('filesystems.partition'))->files($location);
-        $path = null;
-        if (count($files) > 0):
-            foreach ($files as $file):
-                if (strpos($file, 'Large.jpg')):
-                    $path = config('filesystems.disks')[config('filesystems.partition')]['root'] . $file;
-                    break;
-                endif;
-             endforeach;
+        if (! Cache::store('redis')->has('song_photo_' . $id)):
+            $song = Song::find($id);
+            $location = explode(DIRECTORY_SEPARATOR, $song->location);
+            $location = $this->media_directory . $location[0] . DIRECTORY_SEPARATOR . $location[1];
+            $files = Storage::disk(config('filesystems.partition'))->files($location);
+            $path = null;
+            if (count($files) > 0):
+                foreach ($files as $file):
+                    if (strpos($file, 'Large.jpg')):
+                        $path = config('filesystems.disks')[config('filesystems.partition')]['root'] . $file;
+                        Cache::store('redis')->put('song_photo_' . $id, $path, 86400);
+                        break;
+                    endif;
+                 endforeach;
+             endif;
          endif;
 
         if (!$path):
